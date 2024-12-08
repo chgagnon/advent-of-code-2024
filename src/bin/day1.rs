753dc8c642a70bd12313121eb352b10e94246132
Charlie <47061354+chgagnon@users.noreply.github.com>
2024-12-08 14:18:55 -0500

    update day1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,6 +1,7 @@
 use std::fs;
-use std::iter::zip;
 use std::collections::HashMap;
+use std::iter::zip;
+use utils;
 
 fn get_file_contents() -> Vec<Vec<i32>> {
     let file_path = "inputs/day1.txt";
@@ -13,7 +14,7 @@ fn get_file_contents() -> Vec<Vec<i32>> {
 }
 
 fn get_part_1(left_column: &Vec<i32>, right_column: &Vec<i32>) -> i32 {
-    let sum = zip(left_column,right_column).fold(0, |acc, elem| (elem.0 - elem.1).abs() + acc);
+    let sum = zip(left_column, right_column).fold(0, |acc, elem| acc + (elem.0 - elem.1).abs());
     sum
 }
 
@@ -23,7 +24,9 @@ fn get_part_2(left_column: &Vec<i32>, right_column: &Vec<i32>) -> i32 {
         map.insert(*i, *map.get(i).unwrap_or(&0) + 1);
     }
 
-    let similarity = left_column.iter().fold(0, |acc, elem| acc + elem * *map.get(elem).unwrap_or(&0) );
+    let similarity = left_column
+        .iter()
+        .fold(0, |acc, elem| acc + elem * *map.get(elem).unwrap_or(&0));
     similarity
 }
 
