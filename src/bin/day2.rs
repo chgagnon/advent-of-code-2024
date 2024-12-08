62bd1c4e7a884571d4cce356deac4a1aa2304066
Charlie <47061354+chgagnon@users.noreply.github.com>
2024-12-08 15:36:35 -0500

    refactor day 2 for more code reuse

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -7,17 +7,60 @@ enum Direction {
     Decreasing,
 }
 
+fn distance_out_of_range(previous: i32, current: i32) -> bool {
+    let distance = (previous - current).abs();
+    distance < 1 || distance > 3
+}
+
+fn set_or_check_dir(dir: &mut Option<Direction>, previous: i32, current: i32) -> bool {
+    match dir {
+        Some(Direction::Increasing) => {
+            return current >= previous;
+        }
+        Some(Direction::Decreasing) => {
+            return current <= previous;
+        }
+        None => {
+            if current > previous {
+                *dir = Some(Direction::Increasing);
+            } else {
+                *dir = Some(Direction::Decreasing);
+            }
+            return true;
+        }
+    }
+}
+
 fn is_report_safe_1(report: Vec<i32>) -> bool {
-    
+    let mut previous_level = report[0];
+    let mut dir: Option<Direction> = None;
+    for level in &report[1..] {
+        if distance_out_of_range(previous_level, *level) {
+            return false;
+        }
+        if !set_or_check_dir(&mut dir, previous_level, *level) {
+            return false;
+        }
+        previous_level = *level;
+    }
+    return true;
 }
 
 fn is_report_safe_2(report: Vec<i32>) -> bool {
-    let mut previous_level = -1;
+    // If the first level can be removed and the rest of the report is safe,
+    // then the report is safe for part 2.
+    if is_report_safe_1(report[1..].to_vec()) {
+        return true;
+    }
+    // Since the report is not safe when the first level is removed, we can
+    // assume that, even if the report must have a level removed, it will
+    // not be the first level.
+    let mut previous_level = report[0];
     let mut dir: Option<Direction> = None;
     let mut skipped_level_already = false;
-    for level in &report {
-        let distance = (previous_level - level).abs();
-        if distance < 1 || distance > 3 {
+
+    for level in &report[1..] {
+        if distance_out_of_range(previous_level, *level) {
             if skipped_level_already {
                 return false;
             } else {
@@ -26,33 +69,12 @@ fn is_report_safe_2(report: Vec<i32>) -> bool {
             }
         }
 
-        match dir {
-            Some(Direction::Increasing) => {
-                if *level < previous_level {
-                    if skipped_level_already {
-                        return false;
-                    } else {
-                        skipped_level_already = true;
-                        continue;
-                    }
-                }
-            }
-            Some(Direction::Decreasing) => {
-                if *level > previous_level {
-                    if skipped_level_already {
-                        return false;
-                    } else {
-                        skipped_level_already = true;
-                        continue;
-                    }
-                }
-            }
-            None => {
-                if *level > previous_level {
-                    dir = Some(Direction::Increasing);
-                } else {
-                    dir = Some(Direction::Decreasing);
-                }
+        if !set_or_check_dir(&mut dir, previous_level, *level) {
+            if skipped_level_already {
+                return false;
+            } else {
+                skipped_level_already = true;
+                continue;
             }
         }
         previous_level = *level;
@@ -60,15 +82,13 @@ fn is_report_safe_2(report: Vec<i32>) -> bool {
     return true;
 }
 
-fn get_num_safe() -> u32 {
+fn get_num_safe(predicate: fn(Vec<i32>) -> bool) -> u32 {
     let file_path = "inputs/day2.txt";
     let file = File::open(file_path).expect("File read failed");
     let reader = BufReader::new(file).lines();
 
-    let predicate = if use_part_2 {is_report_safe_2} else is_report_safe_1;
-
     let count = reader.fold(0, |acc, line| {
-        acc + is_report_safe_2(
+        acc + predicate(
             line.unwrap()
                 .split(" ")
                 .map(|s| s.parse::<i32>().unwrap())
@@ -78,6 +98,6 @@ fn get_num_safe() -> u32 {
     count
 }
 fn main() {
-    let num_safe = get_num_safe();
+    let num_safe = get_num_safe(is_report_safe_2);
     println!("There are {} safe reports", num_safe);
 }
